646c075b7b314c38b83d827e73f9555228e7cf2e
Farah M <farah.maria.rahman@gmail.com>
2022-10-26 12:38:21 +0000

    import and add SQL Lite with Todo function

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import os
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tsest.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  content = db.Column(db.string(200), nullable = False)
+  date_created = db.Column(db.DateTime, default = datetime.utcnow())
+
+def __repr__(self):
+  return '<Task %f>' % self.id
 
 @app.route("/")
 def index():
@@ -12,4 +24,3 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-    
